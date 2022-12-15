d98c63ae14100a19141d05c82b344008c5846e27
SodamNa <78574152+SodamNa@users.noreply.github.com>
2022-12-14 20:51:29 -0800

    Update PA5.cpp

--- PA5.cpp
+++ PA5.cpp
@@ -2,17 +2,16 @@ class Solution {
 public:
     vector<int> twoSum(vector<int>& nums, int target) {
         unordered_map<int, int> hash;
-	    vector<int> indices;
+	vector<int> indices;
+	    
 	for (int i = 0; i < nums.size(); i++) {
 		int sub = target - nums[i];
 
 		if (hash.find(sub) != hash.end()) {
-
 			indices.push_back(hash[sub]);
 			indices.push_back(i);			
 			return indices;
 		}
-
 		hash[nums[i]] = i;
 	}
 	return indices;
