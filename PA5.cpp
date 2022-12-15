1e96ca2fe0053fd2331dd0fbd084871602d0fa8f
SodamNa <78574152+SodamNa@users.noreply.github.com>
2022-12-14 20:26:01 -0800

    Update PA5.cpp

--- PA5.cpp
+++ PA5.cpp
@@ -1,17 +1,17 @@
 class Solution {
 public:
     vector<int> twoSum(vector<int>& nums, int target) {
-        vector<int> indices;
+        vector<int> hash;
         for(int i = 0; i < nums.size(); i++){
             int sub = target - nums.at(i);
             for(int j = i + 1; j < nums.size(); j++){
                 if(nums.at(j) == sub){
-                    indices.push_back(i);
-                    indices.push_back(j);
+                    hash.push_back(i);
+                    hash.push_back(j);
                     break;
                 }
             }
         }
-        return indices;
+        return hash;
     }
 };
